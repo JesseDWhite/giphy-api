1b30cb3e69f273017c1e07ea20753ec916fc248f
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-04-20 14:42:34 -0800

    add promise methods
    Co-authored-by: Cassandra Copp <cass16180@gmail.com> co-authored-by: Jesse White <jesse.white6@gmail.com>

--- src/main.js
+++ src/main.js
@@ -6,34 +6,39 @@ import './css/styles.css';
 // API URL template: api.giphy.com/v1/gifs/search?api_key=YOUR_API_KEY&limit=1&q=searchterm
 // start with a successful search for just one word
 
-$(document).ready(() => {
-  $('#search-submit-btn').click(() => {
+$(document).ready(function() {
+  $('#search-submit-btn').click(function(event) {
     event.preventDefault();
     let searchTerms = $('#search-terms').val();
     let urlSearchTerms = `&q=${searchTerms}`;
     $('#search-terms').val("");
-    const url = `http://api.giphy.com/v1/gifs/search?&api_key=${process.env.API_KEY}&limit=1${urlSearchTerms}`;
-    console.log(url);
+    let promise = new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      const url = `http://api.giphy.com/v1/gifs/search?&api_key=${process.env.API_KEY}&limit=1${urlSearchTerms}`;
+      console.log(url);
+      
+      console.log(request);
+      
+      request.onload = function () {
+        
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }  
+      request.open("GET", url, true);
+      request.send();
+    });
     
-    let request = new XMLHttpRequest();
-    console.log(request);
-    
-    request.onreadystatechange = function () {
-
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        console.log(response.data[0].images.fixed_height.url);
-        console.log(response.data[0].title);
-        getElements(response);
-      }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response) {
+    promise.then(function(response) {
+      const body = JSON.parse(response);
       $('#search-results').html("");
-      $('#search-results').html(`<img src="${response.data[0].images.fixed_height.url}"> alt="${response.data[0].title}"`).show();
-    }
+      $('#search-results').html(`<img src="${body.data[0].images.fixed_height.url}"> alt="${body.data[0].title}"`).show();
+    },
+    function (error){
+      console.log(error);
+    });
   });
-});
+    
+  });
