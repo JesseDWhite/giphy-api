7c50d8767b3b4f182c8ad94969b164cf8d58328c
Jesse White <jesse.white6@gmail.com>
2021-04-20 16:07:41 -0700

    initial commit.

--- src/main.js
+++ src/main.js
@@ -7,33 +7,33 @@ import './css/styles.css';
 // start with a successful search for just one word
 
 $(document).ready(() => {
-  $('#search-submit-btn').click(() => {
-    event.preventDefault();
-    let searchTerms = $('#search-terms').val();
-    let urlSearchTerms = `&q=${searchTerms}`;
-    $('#search-terms').val("");
-    const url = `http://api.giphy.com/v1/gifs/search?&api_key=${process.env.API_KEY}&limit=1${urlSearchTerms}`;
-    console.log(url);
-    
-    let request = new XMLHttpRequest();
-    console.log(request);
-    
-    request.onreadystatechange = function () {
+    $('#search-submit-btn').click(() => {
+        event.preventDefault();
+        let searchTerms = $('#search-terms').val();
+        let urlSearchTerms = `&q=${searchTerms}`;
+        $('#search-terms').val("");
+        const url = `http://api.giphy.com/v1/gifs/search?&api_key=${process.env.API_KEY}&limit=1${urlSearchTerms}`;
+        console.log(url);
 
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        console.log(response.data[0].images.fixed_height.url);
-        console.log(response.data[0].title);
-        getElements(response);
-      }
-    };
+        let request = new XMLHttpRequest();
+        console.log(request);
 
-    request.open("GET", url, true);
-    request.send();
+        request.onreadystatechange = function () {
 
-    function getElements(response) {
-      $('#search-results').html("");
-      $('#search-results').html(`<img src="${response.data[0].images.fixed_height.url}"> alt="${response.data[0].title}"`).show();
-    }
-  });
+            if (this.readyState === 4 && this.status === 200) {
+                const response = JSON.parse(this.responseText);
+                console.log(response.data[0].images.fixed_height.url);
+                console.log(response.data[0].title);
+                getElements(response);
+            }
+        };
+
+        request.open("GET", url, true);
+        request.send();
+
+        function getElements(response) {
+            $('#search-results').html("");
+            $('#search-results').html(`<img src="${response.data[0].images.fixed_height.url}"> alt="${response.data[0].title}"`).show();
+        }
+    });
 });
